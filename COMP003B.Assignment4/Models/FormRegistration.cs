90b00c897251d8260225da967f18b3e6118ad18f
SalinasJason <jasonsalinas770@gmail.com>
2025-04-09 22:22:32 -0700

    Created a model named FormRegistration

---
+++ COMP003B.Assignment4/Models/FormRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class FormRegistration
+    {
+        [Required]
+        [MinLength(3)]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Range(1, 10)]
+        public int Quantity { get; set; }
+
+        public string DiscountCode { get; set; }
+    }
+}
