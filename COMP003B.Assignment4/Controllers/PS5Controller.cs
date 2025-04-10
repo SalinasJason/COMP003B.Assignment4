769f59b27c0a1fa0280da2e4364d42a87c0c9c1b
SalinasJason <jasonsalinas770@gmail.com>
2025-04-09 22:17:39 -0700

    Added PS5Controller

---
+++ COMP003B.Assignment4/Controllers/PS5Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class PS5Controller : Controller
+    {
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public  IActionResult Games()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Accessries()
+        {
+            return View();
+        }
+    }
+}
