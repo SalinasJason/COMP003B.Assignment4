48c50bc22499788214e1e70b2d61ad8702657f7b
SalinasJason <jasonsalinas770@gmail.com>
2025-04-09 22:28:09 -0700

    Added a Form controller

---
+++ COMP003B.Assignment4/Controllers/FormController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment4.Models;
+
+
+namespace COMP003B.Assignment4.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegisterForm(FormRegistration registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registration);
+            }
+            return RedirectToAction("Success", registration);
+        }
+
+        [HttpGet]
+        public IActionResult Success(FormRegistration registration)
+        {
+            return View(registration);
+        }
+    }
+}
